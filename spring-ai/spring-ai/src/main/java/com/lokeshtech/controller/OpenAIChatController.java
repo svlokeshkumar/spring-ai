bb90acf8c8d485a1875b2dc3de8734fed9fe8a73
Lokesh Kumar <lokeshkumar.vari@gmail.com>
2026-05-17 13:29:41 +0530

    message roles example

--- spring-ai/spring-ai/src/main/java/com/lokeshtech/controller/OpenAIChatController.java
+++ spring-ai/spring-ai/src/main/java/com/lokeshtech/controller/OpenAIChatController.java
@@ -1,8 +1,11 @@
 package com.lokeshtech.controller;
 
+import com.lokeshtech.service.MessageRolesDemoServices;
 import com.lokeshtech.service.OpenAIChatService;
+import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,13 +13,26 @@ import org.springframework.web.bind.annotation.RestController;
 public class OpenAIChatController {
 
     private final OpenAIChatService openAIChatService;
+    private final MessageRolesDemoServices messageRolesDemoServices;
 
-    public OpenAIChatController(OpenAIChatService openAIChatService) {
+    public OpenAIChatController(OpenAIChatService openAIChatService, MessageRolesDemoServices messageRolesDemoServices) {
         this.openAIChatService = openAIChatService;
+        this.messageRolesDemoServices = messageRolesDemoServices;
     }
 
     @GetMapping("/chat")
     public String chat(String message){
         return openAIChatService.chatWithOpenAILLM(message);
+    }
+
+    @GetMapping("/checkPolicy")
+    public String checkInsurancePolicy(@RequestParam String message){
+        return messageRolesDemoServices.checkPolicyv2(message);
+    }
+
+
+    @GetMapping("/checkPolicyv3")
+    public ChatResponse checkInsurancePolicyV3(@RequestParam String message){
+        return messageRolesDemoServices.checkPolicyv3(message);
     }
 }
